1587b00ad5739e15fba09cf01e80ba1d55f03b97
Jamnitzer <wrenholt@inebraska.com>
2015-02-07 00:19:48 -0600

    Folder change
    Resented to include project & read_me

---
+++ Metal_VertexStreamingTests/Metal_VertexStreamingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Metal_VertexStreamingTests.swift
+//  Metal_VertexStreamingTests
+//
+//  Created by Jim Wrenholt on 2/6/15.
+//  Copyright (c) 2015 Nordic Software, Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Metal_VertexStreamingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Metal_VertexStreaming
+
+This is a conversion to Swift of Apple's WWDC 2014 example MetalVertexStreaming.
+
+This sample shows how to stream vertex data between 3 command buffers using one block of memory shared by both the CPU and GPU. The original data is copied back into the Metal shared CPU/GPU buffer every frame and modified directly in the buffer to animate the triangle. 
+
